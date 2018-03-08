a8c8dc6b744d6396ffc49d24d415259c89e87719
Christian <ganjas62@gmail.com>
2018-03-08 11:04:15 +0700

    Ngubah angka

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,8 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Test di sini" );
         int number = 5;
-        System.out.println( number + 10 );
+        number *= 2;
+        System.out.println( number + 15 );
     }
 }
