2ec89e1e8aaacc9a8b35ca2b7c6dda5250d3f20f
DAMAR PRIHADI <damar.prihadi@binus.ac.id>
2018-03-08 11:30:02 +0700

    test gabut

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,5 +12,7 @@ public class App
         int number = 5;
         number *= 2;
         System.out.println( number + 15 );
+	System.out.println( number + 20);
+	System.out.println( number + 20);    
     }
 }
